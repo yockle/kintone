a671fb665dd48a629e55e70e6d24cc9e2488f8ba
yockle <40360157+yockle@users.noreply.github.com>
2018-09-30 15:21:15 +0900

    first commit

---
+++ welcome.js
@@ -0,0 +1,45 @@
+(function () {
+
+    "use strict";
+
+    
+
+    kintone.events.on('app.record.index.show', function(event) {
+
+        window.alert('レコード一覧イベント');
+
+    });
+
+    
+
+    kintone.events.on('app.record.detail.show', function(event) {
+
+        window.alert('レコード表示イベント');
+
+    });
+
+    
+
+    kintone.events.on('app.record.create.show', function(event) {
+
+        window.alert('レコード追加イベント');
+
+    });
+
+    
+
+    kintone.events.on('app.record.edit.show', function(event) {
+
+        window.alert('レコード編集イベント');
+
+    });
+
+    
+
+    kintone.events.on('app.report.show', function(event) {
+
+        window.alert('グラフ表示イベント');
+
+    });
+
+})();
